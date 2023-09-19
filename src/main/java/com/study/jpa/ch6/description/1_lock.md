40ef2169a11231aef77672afd95966f8fa48b0d4
outgrow0905 <danmuzi95@gmail.com>
2023-09-19 10:42:41 +0900

    non repeatable read

--- src/main/java/com/study/jpa/ch6/description/1_lock.md
+++ src/main/java/com/study/jpa/ch6/description/1_lock.md
@@ -37,10 +37,38 @@ void lock1() throws Exception {
 
 ![lock1](img/lock1.png)
  
-맨 위의 쿼리는 `findById()`로 인해 수행된 것이고, 두번째와 세번째 쿼리를 주목할 필요가 있다.    
+맨 위의 쿼리는 `findById()`로 인해 수행된 것이고, `두번째와 세번째 쿼리`를 주목할 필요가 있다.    
 `두번째 쿼리`에서 `update`시에 `set`과 `where` 조건에서 `version`으로 조회하고 `1`을 증가시키는 부분을 볼 수 있다.  
 그리고 세번째로 `select`가 다시 수행되는데 이 부분이 중요하다.  
 `commit`을 하기 전에 `version`이 `8`이 맞는지 확인하는 것이다.  
 그리고 해당 데이터가 다른 쓰레드에 의해 `8`에서 `9`값으로 증가되었기 때문에 그 아래에 `ObjectOptimisticLockingFailureException` 오류가 발생했다.  
 이렇게 동작한다면 위에서 예시로 든 `두 번의 인출문제`를 해결 할 수 있다.  
-동시에 같은 `versino`을 조회했어도 먼저 `commit` 한 것이 반영되고, 다른 하나는 실패하는 전략이다.
+동시에 같은 `versino`을 조회했어도 먼저 `commit` 한 것이 반영되고, 다른 하나는 실패하는 전략이다.  
+
+또한 이 정도의 락 설정은 `NON-REPEATABLE READ`가 발생하는것도 알고 있어야 한다. (락 수준이 높다고 좋은것은 아니다. 개발자의 의도대로 작동하면 된다.)  
+한 트렌젝션 내에서 같은 읽기의 결과값이 다를 수 있는 것이다.  
+
+`NON-REPEATABLE READ`은 아래와 같이 쉽게 재현해볼 수 있다.  
+다만 `isolation level`이 `READ_COMMITTED`보다는 낮게 설정되어있어야 한다.  
+
+~~~java
+@Test
+@Transactional(isolation = Isolation.READ_COMMITTED)
+void lock2() throws Exception {
+    HMemberV1 member = hMemberRepositoryV1.findById(1L).get();
+    log.info("member: {}", member);
+
+    // update on query browser
+    Thread.sleep(10000);
+
+    // clear
+    entityManager.flush();
+    entityManager.clear();
+
+    member = hMemberRepositoryV1.findById(1L).get();
+    log.info("member: {}", member);
+}
+~~~
+같은 `findById()`이지만 다른 결과값을 아래 로그에서 확인할 수 있다.
+
+![lock2](img/lock2.png)
--- src/test/java/com/study/jpa/ch6/v1/enitty/LockV1Test.java
+++ src/test/java/com/study/jpa/ch6/v1/enitty/LockV1Test.java
@@ -1,11 +1,14 @@
 package com.study.jpa.ch6.v1.enitty;
 
 import com.study.jpa.ch6.v1.repository.HMemberRepositoryV1;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
@@ -16,6 +19,9 @@ class LockV1Test {
     @Autowired
     private HMemberRepositoryV1 hMemberRepositoryV1;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Test
     @Transactional
     void lock1() throws Exception {
@@ -24,7 +30,23 @@ class LockV1Test {
         member.setMoney(member.getMoney() - 5);
 
         // update on query browser
-
         Thread.sleep(5000);
+    }
+
+    @Test
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    void lock2() throws Exception {
+        HMemberV1 member = hMemberRepositoryV1.findById(1L).get();
+        log.info("member: {}", member);
+
+        // update on query browser
+        Thread.sleep(10000);
+
+        // clear
+        entityManager.flush();
+        entityManager.clear();
+
+        member = hMemberRepositoryV1.findById(1L).get();
+        log.info("member: {}", member);
     }
 }
